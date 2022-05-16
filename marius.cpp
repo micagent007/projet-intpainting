4abf764399c2dee2a53e13c72b3f7fa2d4af68fe
Marius Michetti <marius.michetti@eleves.enpc.fr>
2022-05-16 14:31:31 +0200

    oue

--- marius.cpp
+++ marius.cpp
@@ -1,7 +1,27 @@
-#include <Imagine/Graphics.h>
-#include<cmath>
-using namespace Imagine;
-#include <iostream>
-using namespace std;
-#include <vector>
-#include "cord.h"
+#include "marius.h"
+
+
+
+
+void calcul_conf(cord p,std::vector<cord> patch,Image<double> conf){
+    double s=0;
+    for(int i=0;i<patch.size();i++){
+        if(p.x!=patch[i].x && p.y!=patch[i].y)
+            s+=conf(patch[i].x,patch[i].y);
+    }
+    s/=patch.size();
+    conf(p.x,p.y)=s;
+}
+void calc_prio(std::vector<pixel_bord> pix,Image<double> conf,std::vector<double> grad,FilePriorite F,int i){
+    pix[i].priorite=conf(pix[i].P.x,pix[i].P.y)*grad[i];
+    F.push(pix[i]);
+}
+
+FilePriorite init_prio(std::vector<pixel_bord> pix, Image<double> conf, std::vector<double> grad){
+    FilePriorite F;
+    for(int i=0;i<pix.size();i++){
+        pix[i].priorite=conf(pix[i].P.x,pix[i].P.y)*grad[i];
+        F.push(pix[i]);
+    }
+    return F;
+}
--- marius.h
+++ marius.h
@@ -7,7 +7,17 @@ using namespace Imagine;
 #include <iostream>
 using namespace std;
 #include <vector>
+#include <Imagine/Images.h>
 #include "cord.h"
+#include "pixel_bord.h"
+#include "priorite.h"
 
 
+std::vector<cord> calc_patch(int type_patch,cord p,Image<double> I)
+
+
+void calcul_conf(cord p,std::vector<cord> patch,Image<double> conf);
+
+void calc_prio(std::vector<pixel_bord> pix,Image<double> conf,std::vector<double> grad,FilePriorite F,int i);
+FilePriorite init_prio(std::vector<pixel_bord> pix, Image<double> conf, std::vector<double> grad);
 #endif // MARIUS_H
