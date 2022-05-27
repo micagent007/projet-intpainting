2a37136e020ffc7e086921dd732bf52a4956d4f9
arthurbr11 <arthurb242@icloud.com>
2022-05-27 17:05:39 +0200

    modif

--- arthur.cpp
+++ arthur.cpp
@@ -105,7 +105,7 @@ void Espace_blanc_compar_blanc(int W, int H,vector <cord> ListePoint ,byte* r, b
         }
 }
 
-bool omega_isnt_empty(int W, int H,Image<double> conf){
+bool omega_is_empty(int W, int H,Image<double> conf){
     for (int x=0;x<W;x++)
         for (int y=0;y<H;y++)
             if (conf(x,y)==0)
@@ -168,8 +168,7 @@ void main_loop(int W, int H,std::vector <cord> ListePoint,byte* r,byte* g,byte* 
     Espace_blanc_compar_blanc(W,H,ListePoint,r,g,b,conf);// initialize the conf (1)
 
     int iter=0;
-    while(omega_isnt_empty(W,H,conf)){//(1a)
-
+    while(!omega_is_empty(W,H,conf)){//(1a)
         if (iter%10==0){
             putColorImage(0,0,r,g,b,W,H);
             click();}
--- arthur.h
+++ arthur.h
@@ -17,7 +17,7 @@ bool in(std::vector <cord> ListePoint,cord P);
 bool autour_blanc(cord P,int W, int H,byte* r, byte* g, byte* b);
 void Espace_blanc_compar_blanc(int W, int H,vector <cord> ListePoint ,byte* r, byte* g, byte* b,Image<double> conf);
 cord find_q(int W,int H,pixel_bord p_max,Image<double> conf,byte* r,byte* g,byte* b);
-bool omega_isnt_empty(int W, int H,Image<double> conf);
+bool omega_is_empty(int W, int H,Image<double> conf);
 void update_conf(cord p,Image<double> conf);
 void copy_image_data(cord q, cord p,Image<double> conf,byte* r,byte* g,byte* b);
 void main_loop(int W,int H,std::vector <cord> ListePoint,byte* r,byte* g,byte* b);
